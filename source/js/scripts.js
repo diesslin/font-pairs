dcc62ab8be3228d08b76fe074136dcb3c57ca400
nicholas <nicholasdiesslin@gmail.com>
2015-09-06 20:09:02 -0500

    converting to vanilla javascript, distching jQuery

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,59 +1,62 @@
 ﻿// This will just kick off everything when the document is ready.
-$(document).ready( function () {
+document.addEventListener("DOMContentLoaded", function() {
   process();
 });
 
 // This function controls at a high level what needs to get run.
 function process() {
+  // Start by retrieving the JSON data from Google.
+  fontObject = JSON.parse( readJSON( 'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyCQqdSotGRoFU331DHXZkt33jFFcB0XrdY' ) );
 
-  // Start by retrieving the data from Google.
-  getGoogleJSON();
-}
+  // Create arrays to hold items
+  var category = [];
+  var font = [];
 
-// Gets the JSON data from Google and returns an object which contains it.
-function getGoogleJSON() {
-
-  $.getJSON("https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyCQqdSotGRoFU331DHXZkt33jFFcB0XrdY",
-  function (data) {
-
-    // Create arrays to hold
-    var category = [];
-    var font = [];
-
-    // Cycle through the returned data, building a distinct list of categories.
-    $.each(data.items, function (key, val) {
-      if ($.inArray(val.category, category) == "-1") {
-        category.push(val.category);
-      }
-    });
-
-    // Cycle through the returned data, building a complete list of fonts (with categories).
-    $.each(data.items, function (key, val) {
-      font.push(val.family);
-    });
-
-    // Call function to create the Category drop down.
-    createCategortyDropDown(category);
-
-    // Call function to create the Font drop down.
-    createFontDropDown(font);
+  // Add All Fonts category to select list
+  category.push( "All Fonts" );
+  // Cycle through the returned data, building a distinct list of categories.
+  fontObject.items.forEach( function ( val ) {
+    // Check if value exists in array or not, this is not supported in IE8
+    if ( category.indexOf( val.category ) == "-1"  ) {
+      category.push( val.category );
+    }
   });
+
+  // Cycle through the returned data, building a complete list of fonts (with categories).
+  fontObject.items.forEach( function ( val ) {
+    font.push( val.family );
+  });
+
+  // Call function to create the Category drop down.
+  createCategortyDropDown( category );
+
+  // Call function to create the Font drop down.
+  createFontDropDown( font );
 }
 
-function createCategortyDropDown(category) {
+// Read JSON file in this case it will be getting google's font JSON
+function readJSON( file ) {
+  var request = new XMLHttpRequest();
+  request.open( 'GET', file, false );
+  request.send( null );
+  if ( request.status == 200 )
+    return request.responseText;
+};
+
+function createCategortyDropDown( category ) {
   // Create an empty array.
   var categoryItems = [];
 
   // Build the corresponding HTML elements for the category dropdown.
-  $.each(category, function (key, val) {
-    categoryItems.push("<option>" + val + "</option>");
+  $.each( category, function ( key, val ) {
+    categoryItems.push( "<option>" + val + "</option>" );
   });
 
   // Append the contents of our array to the body.
-  $("<select>", {
+  $( "<select>", {
     "class": "category-list",
-    html: categoryItems.join("")
-  }).appendTo("body");
+    html: categoryItems.join( "" )
+  }).appendTo( "body" );
 }
 
 function createFontDropDown(font) {
