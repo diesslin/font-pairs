c21a85fc9db91d077c90b321856037acace11f6e
ndiesslin <nicholasdiesslin@gmail.com>
2015-09-10 22:10:36 -0500

    added select change function

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -6,43 +6,53 @@ document.addEventListener("DOMContentLoaded", function() {
 // This function controls at a high level what needs to get run
 function process() {
   // Start by retrieving the JSON data from Google.
-  fontObject = JSON.parse( readJSON( 'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyCQqdSotGRoFU331DHXZkt33jFFcB0XrdY' ) );
+  fetchJSONFile( 'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyCQqdSotGRoFU331DHXZkt33jFFcB0XrdY', function( data ){
+    // Create variable for JSON object
+    fontObject = data;
 
-  // Create arrays to hold items
-  var category = [];
-  var font = [];
+    // Create arrays to hold items
+    var category = [];
+    var font = [];
 
-  // Add All Fonts category to select list
-  category.push( "Select Font Category", "All Fonts" );
-  // Cycle through the returned data, building a distinct list of categories
-  fontObject.items.forEach( function ( val ) {
-    // Check if value exists in array or not, this is not supported in IE8
-    if ( category.indexOf( val.category ) == "-1"  ) {
-      category.push( val.category );
-    }
+    // Add All Fonts category to select list
+    category.push( "Select Font Category", "All Fonts" );
+    // Cycle through the returned data, building a distinct list of categories
+
+    for ( i = 0; i < fontObject.items.length; i ++ ) {
+      val = fontObject.items[i]
+      if ( category.indexOf( val.category ) == "-1"  ) {
+        category.push( val.category );
+      }
+    };
+
+    // Cycle through the returned data, building a complete list of fonts (with categories)
+    for ( i = 0; i < fontObject.items.length; i ++ ) {
+      val = fontObject.items[i]
+      font.push( val.family );
+    };
+
+    // Call function to create the Category drop down
+    createCategortyDropDown( category );
+
+    // Call function to create the Font drop down
+    createFontDropDown( fontObject );
   });
-
-  // Cycle through the returned data, building a complete list of fonts (with categories)
-  fontObject.items.forEach( function ( val ) {
-    font.push( val.family );
-  });
-
-  // Call function to create the Category drop down
-  createCategortyDropDown( category );
-
-  // Call function to create the Font drop down
-  //createFontDropDown( font );
-  createFontDropDown( fontObject );
 }
 
 // Read JSON file in this case it will be getting google's font JSON
-function readJSON( file ) {
-  var request = new XMLHttpRequest();
-  request.open( 'GET', file, false );
-  request.send( null );
-  if ( request.status == 200 )
-    return request.responseText;
-};
+function fetchJSONFile( path, callback ) {
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = function() {
+    if ( httpRequest.readyState === 4 ) {
+      if ( httpRequest.status === 200 ) {
+        var data = JSON.parse( httpRequest.responseText );
+        if ( callback ) callback( data );
+      }
+    }
+  };
+  httpRequest.open( 'GET', path );
+  httpRequest.send();
+}
 
 // Create select list from Category data
 function createCategortyDropDown( category ) {
@@ -50,19 +60,25 @@ function createCategortyDropDown( category ) {
   var categoryItems = [];
 
   // Build the corresponding HTML elements for the category dropdown
-  category.forEach( function ( val ) {
+  for ( i = 0; i < category.length; i ++ ) {
     categoryItems.push( "<option>" + val + "</option>" );
-  });
+  };
 
   // Append the contents of our array to the body
-  selectList( "category-list", category );
+  selectList( "category-list", category, false );
+
+  onChangeInit( "category-list" );
 }
 
 // Create select list from font families data
 function createFontDropDown( font ) {
   // Append the contents of our array to the body
   selectList( "family-list", font, true );
-  document.getElementById("family-list").disabled = true;
+
+  //categoryFilter("family-list")
+
+  // Make list disabled until option is made
+  document.getElementById( "family-list" ).disabled = true;
 }
 
 // Append the contents of our array to the body
@@ -87,4 +103,19 @@ function selectList( listName, array, jsonObject ) {
       select.options[ select.options.length ] = new Option( array[i], array[i] );
     }
   }
+}
+
+// Filter dropdown by selected category
+function categoryFilter( selectId ) {
+  var e = document.getElementById(selectId);
+  var strUser = e.options[e.selectedIndex].value;
+  console.log(strUser)
+}
+
+// On change function for select lists
+function onChangeInit( selectId ) {
+  var stringId = "#" + selectId
+  document.querySelector( stringId ).addEventListener( 'change', function() {
+    console.log(this.value)
+  });
 }
